44a1a75c23c6f509461821f814466a821d25c6b6
wgyde <wesley.gyde@gmail.com>
2018-01-27 15:17:57 -0800

    Scene transitions, animation cleanup

---
+++ Assets/Scripts/Scene/EndConditions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndConditions : MonoBehaviour
+{
+    public string ShellSceneName;
+
+    public void EndGame_Lose()
+    {
+        GotoShell();
+    }
+
+    public void EndGame_Win()
+    {
+        GotoShell();
+    }
+
+    private void GotoShell()
+    {
+        SceneManager.LoadSceneAsync(ShellSceneName);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            GotoShell();
+    }
+}
