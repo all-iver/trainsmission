2e335a6f244155d1263545b6e657121cb235d988
Kavan Bahrami <kavanbahrami@gmail.com>
2018-01-27 13:29:10 -0800

    background udpated, pacer movement

---
+++ Assets/Scripts/PacerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacerController : MonoBehaviour {
+    
+    public float scrollRate;
+	
+	void Update () {
+       float xMove = Time.deltaTime * scrollRate;
+        transform.Translate(xMove, 0, 0);
+       // transform.Translate(0, Time.deltaTime, 0, Space.World);
+	}
+}
