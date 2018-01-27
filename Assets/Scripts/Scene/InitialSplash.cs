3e63c076d7f3df66aec79eb2cb0f7b0fce21646e
wgyde <wesley.gyde@gmail.com>
2018-01-27 14:46:48 -0800

    Initial Splash Screen

---
+++ Assets/Scripts/Scene/InitialSplash.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitialSplash : MonoBehaviour
+{
+    public string GameSceneName;
+    public Animator Animator;
+
+    private bool FadeInFinished;
+    private bool Loading;
+
+    public void OnFadeInFinished()
+    {
+        FadeInFinished = true;
+    }
+
+    public void OnFadeOutFinished()
+    {
+        BeginLoad();
+    }
+
+    private void BeginLoad()
+    {
+        Animator.SetTrigger("FadeOut");
+        var op = SceneManager.LoadSceneAsync(GameSceneName);
+        Loading = true;
+        op.completed += OnLoadFinished;
+    }
+
+    private void OnLoadFinished(AsyncOperation op)
+    {
+    }
+
+    private void Start()
+    {
+        Animator.SetTrigger("FadeIn");
+    }
+
+    private void Update()
+    {
+        if (FadeInFinished && Input.anyKeyDown)
+            Animator.SetTrigger("FadeOut");
+    }
+}
