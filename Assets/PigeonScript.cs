0713774c4967469d126c8af1473dcfa1f3b151e7
Kavan Bahrami <kavanbahrami@gmail.com>
2018-01-28 00:43:11 -0800

    stuff

---
+++ Assets/PigeonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigeonScript : MonoBehaviour {
+
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            animator.SetBool("Open", true);
+        }
+    }
+
+}
