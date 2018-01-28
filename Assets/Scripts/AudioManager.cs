d8cd3f73e0c7e42855f292b9c94201a7aaa993d0
Kavan Bahrami <kavanbahrami@gmail.com>
2018-01-28 14:03:28 -0800

    Audio Updates

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioMixerSnapshot mainAudio;
+    public AudioMixerSnapshot outsideAudio;
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("outside");
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("player is outside");
+            outsideAudio.TransitionTo(1f);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("leaving outside");
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("player going inside");
+            mainAudio.TransitionTo(1f);
+        }
+    }
+}
---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioTrigger : MonoBehaviour
+{
+    public AudioSource soundTrigger;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger Entered)");
+        if (collision.CompareTag("Player"))
+        {
+            soundTrigger.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Trigger Exit)");
+        if (collision.CompareTag("Player"))
+        {
+            soundTrigger.Stop();
+        }
+                  
+    }
+}
