58f4852d7c3f097275bcb7c216adf6f431b919f4
Kavan Bahrami <kavanbahrami@gmail.com>
2018-01-28 02:27:31 -0800

    Bird

--- Assets/Scripts/PigeonCagedScript.cs
+++ Assets/Scripts/PigeonCagedScript.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PigeonScript : MonoBehaviour {
+public class PigeonCagedScript : MonoBehaviour {
 
     Animator animator;
     public GameObject pigeon;
+    private bool triggered = false;
 
     void Start()
     {
@@ -24,13 +25,15 @@ public class PigeonScript : MonoBehaviour {
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (Input.GetKeyDown("space"))
+        if (triggered == false)
         {
-            animator.SetBool("Empty", true);
-
-            print("space key was pressed");
-            GameObject FlappyBird = Instantiate(pigeon, transform.position, transform.rotation) as GameObject;
+            if (Input.GetKeyDown("space"))
+            {
+                animator.SetBool("Empty", true);
+                print("space key was pressed");
+                GameObject FlappyBird = Instantiate(pigeon, transform.position, transform.rotation) as GameObject;
+                triggered = true;
+            }
         }
-        
     }
 }
---
+++ Assets/Scripts/PigeonScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PigeonScript : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // Grab a free Sequence to use
+        Sequence mySequence = DOTween.Sequence();
+        // Add a movement tween at the beginning
+        mySequence.Append(transform.DOMove(new Vector3(-18, 2, 0), 5f));
+       // mySequence.Append(transform.DOMoveX(-20, 4f));
+        // Add a rotation tween as soon as the previous one is finished
+        mySequence.Append(transform.DORotate(new Vector3(0, 180, 0), 1));
+        // Delay the whole Sequence by 1 second
+        mySequence.PrependInterval(1);
+        mySequence.Append(transform.DORotate(new Vector3(0, -180, 0), 1));
+        mySequence.Append(transform.DOMove(new Vector3(-30, 4, 0), 10f));
+
+    }
+       
+
+//Sequence mySequence = DOTween.Sequence();
+   //     mySequence.Append(transform.DOMoveX(45, 1))
+    //      .Append(transform.DORotate(new Vector3(0, 180, 0), 1))
+      //    .PrependInterval(1)
+    //      .Insert(0, transform.DOScale(new Vector3(3, 3, 3), mySequence.Duration()));
+
+
+}
