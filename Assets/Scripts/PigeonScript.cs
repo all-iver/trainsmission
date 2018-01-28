6af59694eb8a7fae2c8c614166e92a55834d32bc
Kavan Bahrami <kavanbahrami@gmail.com>
2018-01-28 11:45:34 -0800

    FlappyBird

--- Assets/Scripts/PigeonScript.cs
+++ Assets/Scripts/PigeonScript.cs
@@ -5,10 +5,35 @@ using DG.Tweening;
 
 public class PigeonScript : MonoBehaviour {
 
+    private GameObject player;
+    private Transform pigeon;
+
+    //public GameObject birdBounds;
+    public GameObject newPlayer;
+
+   // Sprite playerSprite;
+   // SpriteRenderer pigeonSpriteRend;
+
+    Animator playerAnimator;
+    Animator pigeonAnimator;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        pigeon = gameObject.transform;
+
+       // playerSprite = player.GetComponent<SpriteRenderer>().sprite;
+       //pigeonSpriteRend = gameObject.GetComponent<SpriteRenderer>();
+
+        playerAnimator = player.GetComponent<Animator>();
+        pigeonAnimator = gameObject.GetComponent<Animator>();
+    }
+
 
     // Use this for initialization
     void Start()
     {
+        
         // Grab a free Sequence to use
         Sequence mySequence = DOTween.Sequence();
         // Add a movement tween at the beginning
@@ -18,10 +43,36 @@ public class PigeonScript : MonoBehaviour {
         mySequence.Append(transform.DORotate(new Vector3(0, 180, 0), 1));
         // Delay the whole Sequence by 1 second
         mySequence.PrependInterval(1);
-        mySequence.Append(transform.DORotate(new Vector3(0, -180, 0), 1));
-        mySequence.Append(transform.DOMove(new Vector3(-30, 4, 0), 10f));
-
+        //mySequence.Append(transform.DORotate(new Vector3(0, -180, 0), 1));
+        //mySequence.Append(transform.DOMove(new Vector3(-30, 4, 0), 10f));
+        StartCoroutine(ChangePlayer(pigeon));
     }
+    
+    
+    IEnumerator ChangePlayer (Transform target)
+    {
+
+        Debug.Log("pigeon = "+ pigeon);
+        Debug.Log("player = " + player);
+
+        Debug.Log("pigeonANIM = " + pigeonAnimator);
+        Debug.Log("playerANIM = " + playerAnimator);
+        yield return new WaitForSeconds(8f);
+
+        Vector3 currentPos = gameObject.transform.position;
+        Debug.Log("currentPos :" + currentPos);
+
+        GameObject bodyDouble = Instantiate(newPlayer,player.transform.position,player.transform.rotation) as GameObject;
+        player.transform.position = currentPos;
+
+        //playerSprite = pigeonSprite;
+        playerAnimator.runtimeAnimatorController = pigeonAnimator.runtimeAnimatorController;
+
+
+        //birdBounds.SetActive(true);
+        pigeon.gameObject.SetActive(false);
+    }
+        
 }
 
 
