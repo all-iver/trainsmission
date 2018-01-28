ccbbcf2d3e0cc00af89b6fe9fd789b66a050ef22
Kavan Bahrami <kavanbahrami@gmail.com>
2018-01-28 15:34:53 -0800

    flappy / audio

---
+++ Assets/Scripts/FlappyAudioSwitch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class FlappyAudioSwitch : MonoBehaviour
+{
+
+    public AudioMixerSnapshot FlappyAudio;
+    public AudioSource FlappySource;
+
+    public void Flappy()
+    {
+        FlappySource.Play();
+        FlappyAudio.TransitionTo(6f);
+    }
+}
