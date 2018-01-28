e51313ba10774f6325d2d63a6a7f3bd3a72ca3ba
wgyde <wesley.gyde@gmail.com>
2018-01-28 11:18:35 -0800

    NPC work

---
+++ Assets/Wesley/NPCSpriteCatalog_Nobodies.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Custom/Nobodies")]
+public class NPCSpriteCatalog_Nobodies : ScriptableObject
+{
+    public static NPCSpriteCatalog_Nobodies Inst;
+    public Sprite[] Sprites;
+}
