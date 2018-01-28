01f1464f228ff8eac601676dd311116969f91082
Kavan Bahrami <kavanbahrami@gmail.com>
2018-01-27 22:53:04 -0800

    smoke

---
+++ Assets/Scripts/LadderScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class LadderScript : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("PlayerHitsLadder");
+        }
+    }
+}
