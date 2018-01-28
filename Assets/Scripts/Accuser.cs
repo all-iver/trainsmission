8df12be15758a3f73cc2fa58735066438999e7c6
Oliver Smith <oliver@sub-c.org>
2018-01-28 13:06:08 -0800

    Better npc speech triggers

--- Assets/Scripts/Accuser.cs
+++ Assets/Scripts/Accuser.cs
@@ -13,36 +13,55 @@ public class Accuser : MonoBehaviour {
         return accusing;
     }
 
-    // void AccuseNearest() {
-    //     foreach (Collider2D coll in Physics2D.OverlapBoxAll((Vector2) transform.position, new Vector2(1, 1), 0)) {
-    //         NPC npc = coll.gameObject.GetComponent<NPC>();
-    //         if (!npc)
-    //             continue;
-    //         // Debug.Log(npc
-    //     }
-    // }
-
-    void OnTriggerEnter2D(Collider2D other) {
+    void TalkToNearest() {
+        NPC nearest = null;
+        float dist = 5235231532;
+        foreach (Collider2D coll in Physics2D.OverlapBoxAll((Vector2) transform.position + new Vector2(0, 1), new Vector2(1, 2), 0)) {
+            NPC npc = coll.gameObject.GetComponent<NPC>();
+            if (!npc)
+                continue;
+            float ndist = Vector2.Distance(transform.position, npc.transform.position);
+            if (!nearest || ndist < dist) {
+                nearest = npc;
+                dist = ndist;
+            }
+        }
+        if (!nearest) {
+            if (current) {
+                current.EndSpeaking();
+                current = null;
+            }
+            return;
+        }
+        if (nearest == current)
+            return;
         if (current)
-            return;
-        NPC npc = other.gameObject.GetComponent<NPC>();
-        if (!npc)
-            return;
-        current = npc;
+            current.EndSpeaking();
+        current = nearest;
         current.StopAndSpeak(transform.position);
     }
 
-    void OnTriggerExit2D(Collider2D other) {
-        if (accusing)
-            return;
-        NPC npc = other.gameObject.GetComponent<NPC>();
-        if (!npc)
-            return;
-        if (npc == current) {
-            current.EndSpeaking();
-            current = null;
-        }
-    }
+    // void OnTriggerEnter2D(Collider2D other) {
+    //     if (current)
+    //         return;
+    //     NPC npc = other.gameObject.GetComponent<NPC>();
+    //     if (!npc)
+    //         return;
+    //     current = npc;
+    //     current.StopAndSpeak(transform.position);
+    // }
+
+    // void OnTriggerExit2D(Collider2D other) {
+    //     if (accusing)
+    //         return;
+    //     NPC npc = other.gameObject.GetComponent<NPC>();
+    //     if (!npc)
+    //         return;
+    //     if (npc == current) {
+    //         current.EndSpeaking();
+    //         current = null;
+    //     }
+    // }
 
     IEnumerator EndGame() {
         yield return new WaitForSeconds(4);
@@ -61,6 +80,9 @@ public class Accuser : MonoBehaviour {
     }
 
     void Update() {
+        if (accusing)
+            return;
+        TalkToNearest();
         if (Input.GetButtonDown("Jump"))
             AccuseCurrent();
     }
