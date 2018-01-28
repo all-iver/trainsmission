e9dfb325652b2f8797ef7bfc559f5729ff8da490
Kavan Bahrami <kavanbahrami@gmail.com>
2018-01-27 21:47:02 -0800

    updates

---
+++ Assets/k_folder/k_scripts/BuildTrain.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildTrain : MonoBehaviour {
+
+    public GameObject[] trains;
+    public GameObject caboose;
+    public GameObject engine;
+    public GameObject trainsHolder;
+
+
+    private GameObject currentTrain;
+
+
+    private void Start ()
+    {
+        BuildChooChoo();
+    }
+
+    void BuildChooChoo(){
+
+        float size = Random.Range(3,8);
+
+        for (int i = 0; i < size; i++)
+        {
+            currentTrain = trains[Random.Range(0, Elements.Length)];
+        }
+
+    }
+}
