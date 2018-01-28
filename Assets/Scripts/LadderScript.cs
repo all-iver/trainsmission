fe6544a89707072966e27211ebefb1cfcb600633
Kavan Bahrami <kavanbahrami@gmail.com>
2018-01-27 23:25:40 -0800

    titles , speech bubbles

--- Assets/Scripts/LadderScript.cs
+++ Assets/Scripts/LadderScript.cs
@@ -3,11 +3,43 @@
 public class LadderScript : MonoBehaviour
 {
 
-    void OnTriggerEnter(Collider other)
+    Animator animator;
+
+    private void Update()
     {
-        if (other.CompareTag("Player"))
+        Debug.Log(Input.GetAxis("Vertical"));
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && (Input.GetAxis("Vertical") > 0.0f))
         {
-            Debug.Log("PlayerHitsLadder");
+            Debug.Log("PlayerHitsLadder and moves up");
+            animator = collision.GetComponent<Animator>();
+            animator.SetBool("Climbing", true);
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && (Input.GetAxis("Vertical") > 0.0f))
+        {
+            Debug.Log("PlayerStaysonLadder and moves up");
+            animator = collision.GetComponent<Animator>();
+            animator.SetBool("Climbing", true);
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("PlayerExitsLadder");
+            animator = collision.GetComponent<Animator>();
+            animator.SetBool("Climbing", false);
+        }
+        
     }
 }
