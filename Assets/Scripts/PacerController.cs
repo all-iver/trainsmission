042acdd713646e9d463d239515e639ce4ca32f24
Oliver Smith <oliver@sub-c.org>
2018-01-27 19:11:20 -0800

    y fixes

--- Assets/Scripts/PacerController.cs
+++ Assets/Scripts/PacerController.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class PacerController : MonoBehaviour {
     
     public float scrollRate;
+    public Transform player;
+    public float yOffset = 0.15f;
 	
 	void Update () {
-       float xMove = Time.deltaTime * scrollRate;
-        transform.Translate(xMove, 0, 0);
-       // transform.Translate(0, Time.deltaTime, 0, Space.World);
+        float xMove = Time.deltaTime * scrollRate;
+        // transform.Translate(xMove, 0, 0);
+        // transform.Translate(0, Time.deltaTime, 0, Space.World);
+        Vector3 p = transform.position;
+        p.x += xMove;
+        p.y = player.position.y + yOffset;
+        transform.position = p;
 	}
 }
