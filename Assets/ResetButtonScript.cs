b807b5819227d1b6cb4a694146b2263aa6346ec9
Kavan Bahrami <kavanbahrami@gmail.com>
2018-01-28 17:07:14 -0800

    restart button

---
+++ Assets/ResetButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ResetButtonScript : MonoBehaviour {
+
+    public void Reset()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("title");
+    }
+}
--- Assets/Scripts/FlappyAudioSwitch.cs
+++ Assets/Scripts/FlappyAudioSwitch.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class FlappyAudioSwitch : MonoBehaviour
 {
 
     public AudioMixerSnapshot FlappyAudio;
     public AudioSource FlappySource;
+    public GameObject resetButton;
 
     public void Flappy()
     {
+        resetButton.SetActive(true);
         FlappySource.Play();
         FlappyAudio.TransitionTo(6f);
     }
