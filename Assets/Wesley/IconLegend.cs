e315b4905e225e287b641c6628951d0fb397384f
wgyde <wesley.gyde@gmail.com>
2018-02-08 22:14:24 -0800

    Added missing icon to legend

---
+++ Assets/Wesley/IconLegend.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class IconLegend : MonoBehaviour
+{
+#pragma warning disable 649
+	[SerializeField] GameObject Root;
+#pragma warning restore 649
+
+	void Update()
+	{
+		Root.SetActive(Input.GetKey(KeyCode.X));
+	}
+}
